15d1bea36b32b7f5843f23c166ba77508bb25977
Su Yee Myat Moe <suyeemyatmoe21@gmail.com>
2024-06-13 18:19:12 +0630

    Ado.Net (Update, Delete and AfterUpdate)

--- SYMMDotNetCore.ConsoleApp/AdoDotNetEExample.cs
+++ SYMMDotNetCore.ConsoleApp/AdoDotNetEExample.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection.Metadata;
 
 namespace SYMMDotNetCore.ConsoleApp
 {
@@ -49,6 +50,41 @@ namespace SYMMDotNetCore.ConsoleApp
                 Console.WriteLine("--------------------------------- ");
             }
         }
+
+        public void AfterUpdate(int id)// similar like read but only show for only one row
+        {
+            SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+            
+            connection.Open();
+            Console.WriteLine("The Connection is successfully opened");
+
+            string query = "select * from Blog_TBL WHERE Blog_ID=@Blog_ID";
+            SqlCommand cmd = new SqlCommand(query, connection);//connection include the data from stringbuilder object 
+            cmd.Parameters.AddWithValue("@Blog_Id", id);// To accept parameter
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);// to create query
+            DataTable dt = new DataTable();//Table
+            sqlDataAdapter.Fill(dt);// fill in the table
+
+            connection.Close();
+            Console.WriteLine("The Connection is successfully closed");
+
+            if (dt.Rows.Count == 0)//to handle unexisting id
+            {
+                Console.WriteLine("No data is found");
+                return;
+            }
+
+            DataRow dr = dt.Rows[0];// since id is unique, data will be only in 0 index
+            {
+                Console.WriteLine("BlogID " + dr["Blog_Id"]);
+                Console.WriteLine("BlogTile " + dr["Blog_Title"]);
+                Console.WriteLine("BlogAuthor " + dr["Blog_Author"]);
+                Console.WriteLine("BlogContent " + dr["Blog_Content"]);
+                Console.WriteLine("--------------------------------- ");
+            }
+
+        }
+
         public void Create(string title,string author, string content)
         {
             SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
@@ -73,6 +109,49 @@ namespace SYMMDotNetCore.ConsoleApp
 
             string message = result > 0 ? "Saving success" : "Saving failed";
             Console.WriteLine(message);
+        }
+        public void Update(int id,string title, string author, string content)
+        //existance of id is important to make sure which row will be updated
+        {
+            SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+            string insertquery = @"UPDATE [dbo].[Blog_TBL]
+   SET [Blog_Title] = @Blog_Title
+      ,[Blog_Author] = @Blog_Author
+      ,[Blog_Content] = @Blog_Content
+ WHERE Blog_ID= @Blog_Id";
+            //To insert with parameter
+
+            SqlCommand cmd = new SqlCommand(insertquery, connection);
+            cmd.Parameters.AddWithValue("@Blog_Id", id);
+            cmd.Parameters.AddWithValue("@Blog_Title", title);// Parameter , variable
+            cmd.Parameters.AddWithValue("@Blog_Author", author);
+            cmd.Parameters.AddWithValue("@Blog_Content", content);
+            int result = cmd.ExecuteNonQuery();// To execute the insertquery code which will return the no of row effected in int type
+
+            connection.Close();
+
+            string message = result > 0 ? "The updating is successful" : "The updating is failed";
+            Console.WriteLine(message);
+
+        }
+        public void Delete(int id) // can delete by choosing Id number
+        {
+            SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+            string insertquery = @"DELETE FROM [dbo].[Blog_TBL]
+      WHERE Blog_ID=@Blog_ID";
+            //To insert with parameter
+
+            SqlCommand cmd = new SqlCommand(insertquery, connection);
+            cmd.Parameters.AddWithValue("@Blog_Id", id);
+            int result = cmd.ExecuteNonQuery();// To execute the insertquery code which will return the no of row effected in int type
+
+            connection.Close();
+
+            string message = result > 0 ? "The deleting process is successful" : "The deleting process is failed";
+            Console.WriteLine(message);
+
         }
     }
 }
--- SYMMDotNetCore.ConsoleApp/Program.cs
+++ SYMMDotNetCore.ConsoleApp/Program.cs
@@ -6,7 +6,10 @@ Console.WriteLine("Hello, World!");
 
 AdoDotNetEExample adoDotNetExample = new AdoDotNetEExample();
 //adoDotNetExample.Read();
-adoDotNetExample.Create("title", "author", "content");
-
+//adoDotNetExample.Create("title", "author", "content");
+//adoDotNetExample.Update(5, "Java & C#", "Pu Pu", "Basic to Advanced");//in update Id increment number can be change
+//adoDotNetExample.Delete(1);
+adoDotNetExample.AfterUpdate(1);
+adoDotNetExample.AfterUpdate(5);
 
 Console.ReadKey();
